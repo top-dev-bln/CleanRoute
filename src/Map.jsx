1a2aa7c4b38f88abee0ac0b1fe4e5dbf4c255dfd
top.dev.bln <top.dev.bln@gmail.com>
2025-03-23 03:28:45 +0200

    makrer

--- src/Map.jsx
+++ src/Map.jsx
@@ -114,9 +114,7 @@ const Map = () => {
       const marker = L.marker(start, { draggable: true,icon: L.icon({
         iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
         iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
+
       })})
         .addTo(mapInstance.current)
         .bindPopup(type === 'start' ? "Start" : "End")
