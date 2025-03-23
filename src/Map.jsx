0bd5e5ccd1b4ab820afbf783990b962827156bc2
top.dev.bln <top.dev.bln@gmail.com>
2025-03-23 02:51:56 +0200

    nou

--- src/Map.jsx
+++ src/Map.jsx
@@ -3,15 +3,15 @@ import L from 'leaflet';
 import axios from 'axios';
 import 'leaflet/dist/leaflet.css';
 import debounce from 'lodash.debounce';
-import { getUserLocation, getAddressFromCoords } from './locationUtils';
+import { getUserLocation, getAddressFromCoords } from '../utils/locationUtils';
 import { SearchBar } from './map/SearchBar';
-
+import { TransportControls } from './map/TransportControls';
 import { AirQualityControl } from './map/AirQualityControl';
 import { AirQualityLegend } from './map/AirQualityLegend';
-import { displayAirSensorsOnMap } from './airQualityUtils';
+import { getAQIColor, displayAirSensorsOnMap } from '../utils/airQualityUtils';
 
 // Server API URL - update this to your actual server URL
-const API_URL = 'https://cleanroute-api.vercel.app/api';
+const API_URL = 'http://localhost:5001/api';
 
 const Map = () => {
   // Core refs
